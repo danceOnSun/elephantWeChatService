dd9cd3e35263528874bbf1aef18e09ee71f6a324
wb-yxl267015 <wb-yxl267015@alibaba-inc.com>
2018-06-05 11:34:08 +0800

    modified

--- springboot/src/main/java/com/jas/controller/AreaController.java
+++ springboot/src/main/java/com/jas/controller/AreaController.java
@@ -2,31 +2,34 @@ package com.jas.controller;
 
 import com.jas.domain.Area;
 import com.jas.service.AreaService;
+import groovy.util.logging.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author xiangle.yu
  * @create 2018-06-01 15:39
  **/
+@Slf4j
 @RestController
 @RequestMapping("/superadmin")
 public class AreaController {
     
     @Autowired
     private AreaService areaService;
-    
+    private final static Logger logger = LoggerFactory.getLogger(AreaController.class);
+
     @GetMapping(value = "/listarea")
     public Map<String, Object> listArea(){
         Map<String, Object> modelMap = new HashMap<>();
         List<Area> list = areaService.getAreaList();
-        
+        Date date=new Date();
         modelMap.put("areaList", list);
-        
+        logger.info(date+":xiangle.yu log test01");
         return modelMap;
     }
     
